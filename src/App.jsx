fac3ccbba36dee4e33a8cbd2b2023416b3b8f0dc
renan-almeida <renanalmeidadantas2007@gmail.com>
2025-07-06 18:22:55 -0300

    feat: funcionalidade de adição do corpo de nova tarefa parcialmente finalizado.

--- src/App.jsx
+++ src/App.jsx
@@ -41,13 +41,23 @@ function App() {
       const newTasks = tasks.filter((task) => task.id !== taskId);
       setTasks(newTasks);
     }
+
+    function onAddTask(title, description) {
+      const newTasks = {
+        id: tasks.length + 1,
+        title: title,
+        description: description,
+        isCompleted: false
+      }
+      setTasks([...tasks, newTasks]);
+    }
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-      <AddTask />
+      <AddTask onAddTask={onAddTask} />
       <ListTasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,14 +1,25 @@
-function AddTask() {
+import  { useState } from "react";
+
+function AddTask({ onAddTask}) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     return (
-        <div className="bg-slate-200 p-6 rounded-md shadow flex flex-col gap-4">
+        <div className="space-y-4 bg-slate-200 p-6 rounded-md shadow flex flex-col gap-4">
             <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
                 type="text" 
-                placeholder="Digite o título da tarefa" />
+                placeholder="Digite o título da tarefa" 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}/>
+
             <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
                 type="text" 
-                placeholder="Digite a descrição da tarefa" />
-                <button className="bg-slate-400 text-white rounded-md px-4 py-2 hover:bg-slate-500 transition-colors duration-300 cursor-pointer">
-                    Adicionar Tarefa
+                placeholder="Digite a descrição da tarefa"
+                value={description} 
+                onChange={(e) => setDescription(e.target.value) } />
+                
+                <button className="bg-slate-500 text-white rounded-md px-4 py-2 font-medium hover:bg-slate-600 transition-colors duration-300 cursor-pointer
+                " onClick={() => onAddTask(title, description)}>
+                   Adicionar Tarefa
                 </button>
         </div>
     )
