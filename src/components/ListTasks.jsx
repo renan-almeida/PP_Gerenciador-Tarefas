b39b9eacfd99bec2b2b5afb600be2eb9a894dccd
renan-almeida <renanalmeidadantas2007@gmail.com>
2025-07-06 21:43:14 -0300

    feat: navegação para a page de detalhes da tarefa criada.

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,6 +1,12 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function ListTasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        navigate(`/task?title=${task.title}&description=${task.description}`);
+    }
     return (
         <ul className="space-y-2 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
@@ -13,7 +19,8 @@ function ListTasks({ tasks, onTaskClick, onDeleteTaskClick }) {
                     {task.title}
                     </button>
                 <button className="bg-slate-400 p-2 text-white rounded-md ml-2
-                    hover:bg-slate-500 transition-colors duration-300">
+                    hover:bg-slate-500 transition-colors duration-300"
+                    onClick={() => onSeeDetailsClick(task)}>
                     <ChevronRightIcon />
                 </button>
                  <button className='bg-slate-400 p-2 text-white rounded-md ml-2
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider } from "react-router-dom";
+import TaskDetails from './pages/TaskDetails.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/task",
+    element: <TaskDetails />
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
